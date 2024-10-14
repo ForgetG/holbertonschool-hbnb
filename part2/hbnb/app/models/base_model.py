1d8859c26d48f93051976fd6f75199c95fef07ac
Forget Guillaume <9430@holbertonstudents.com>
2024-10-14 15:17:19 +0200

    Created base-model file + added User and place class

--- part2/hbnb/app/models/base_model.py
+++ part2/hbnb/app/models/base_model.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""Module user business logic class
+"""Module base_model
 """
 import uuid
 from datetime import datetime
-from .base_model import BaseModel
 
-class User:
-    def __init__(self, first_name, last_name, email, is_admin):
-        super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
-        self.places = [] # list to store related places
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """Update the updated_at timestamp whenever the object is modified"""
--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -1,3 +1,38 @@
 #!/usr/bin/python3
 """Module place business logic class
 """
+import uuid
+from datetime import datetime
+from .base_model import BaseModel
+
+
+class Place(BaseModel):
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.reviews = []  # List to store related reviews
+        self.amenities = []  # List to store related amenities
+
+    def save(self):
+        """Update the updated_at timestamp whenever the object is modified"""
+        self.updated_at = datetime.now()
+
+    def update(self, data):
+        """Update the attributes of the object based on the provided dictionary"""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()  # Update the updated_at timestamp
+
+    def add_review(self, review):
+        """Add a review to the place."""
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        """Add an amenity to the place."""
+        self.amenities.append(amenity)
