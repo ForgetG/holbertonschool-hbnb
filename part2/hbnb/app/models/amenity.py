f463aa0890fd514278a0ecc6d56b848a2264688a
jydzip <dckjeremy.zip@gmail.com>
2024-10-15 09:38:52 +0200

    Add models Review and Amenity

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 """Module amenity business logic class
 """
+from .base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def save(self):
+        """Update the updated_at timestamp whenever the object is modified"""
+        super().save()
+
+    def update(self, data):
+        """Update the attributes of the object based on the provided dictionary"""
+        super().update(data)
--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 """Module review business logic class
 """
+from .base_model import BaseModel
+
+
+class Review(BaseModel):
+    def __init__(self, text, rating):
+        super().__init__()
+        self.text = text
+        self.rating = rating
+
+        self.place = None # store related place
+        self.user = None # store related user
+
+    def save(self):
+        """Update the updated_at timestamp whenever the object is modified"""
+        super().save()
+
+    def update(self, data):
+        """Update the attributes of the object based on the provided dictionary"""
+        super().update(data)
