2a8bbb18893a515391865915ded5e6df33d26f0d
Forget Guillaume <g.forget1999@gmail.com>
2024-10-15 16:34:08 +0200

    implementation of amenity methods

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -11,21 +11,39 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
+        # Placeholder method for creating a user
         user = User(**user_data)
         self.user_repo.add(user)
         return user
 
-    # Placeholder method for fetching a user by ID
     def get_user(self, user_id):
+        # Placeholder method for fetching a user by ID
         return self.user_repo.get(user_id)
 
-    # Placeholder method for fetching a user by email
     def get_user_by_email(self, email):
+        # Placeholder method for fetching a user by email
         return self.user_repo.get_by_attribute('email', email)
 
-    # Placeholder method for fetching a place by ID
+    def create_amenity(self, amenity_data):
+        # Placeholder for logic to create an amenity
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+
+    def get_amenity(self, amenity_id):
+        # Placeholder for logic to retrieve an amenity by ID
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        # Placeholder for logic to retrieve all amenities
+        return list(self.amenity_repo.values())
+
+    def update_amenity(self, amenity_id, amenity_data):
+        # Placeholder for logic to update an amenity
+        obj = self.get(amenity_id)
+        if obj:
+            obj.update(amenity_data)
+
     def get_place(self, place_id):
-        # Logic will be implemented in later tasks
+        # Placeholder method for fetching a place by ID
         pass
