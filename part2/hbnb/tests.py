c1a74d7f47733ce849ed52b65b040a32ef5979dc
ForgetG <g.forget1999@gmail.com>
2024-10-23 17:17:22 +0200

    added Unittest

---
+++ part2/hbnb/tests.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Module tests
+"""
+import unittest
+from app import create_app
+
+class TestUserEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_user(self):
+        response_user = self.client.post('/api/v1/users/', json={
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "email": "jane.doe@example.com"
+        })
+        self.assertEqual(response_user.status_code, 201)     
+
+    def test_create_user_invalid_data(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "",
+            "last_name": "",
+            "email": "invalid-email"
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Invalid input data', response.get_json()['error'])
+
+class TestAmenityEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_amenity(self):
+        response = self.client.post('/api/v1/amenities/', json={
+            "name": "Wifi"
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_amenity_invalid_data(self):
+        response = self.client.post('/api/v1/amenities/', json={
+            "name": ""
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Invalid input data', response.get_json()['error'])
+
+if __name__ == '__main__':
+    unittest.main()
